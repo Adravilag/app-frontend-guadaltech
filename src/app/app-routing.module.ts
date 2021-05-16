0f077ac9d3db4164870913a307ef99bf04a4af84
adravilag <adrian.davilaguerra@gmail.com>
2021-05-16 17:46:25 +0200

    Aplicación lanzada en producción

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,5 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { PersonasComponent } from './pages/personas/personas.component';
-import { BecariosComponent } from './pages/becarios/becarios.component';
-import { PagesComponent } from './pages/pages.component';
-import { HomeComponent } from './pages/home/home.component';
 import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
 import { PagesRoutingModule } from './pages/pages.routing';
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  base_url_personas: "http://localhost:3000/api",
+  base_url_personas: "https://guadaltech-app-prueba.herokuapp.com/api",
   base_url_becarios: "https://guadaltech-fullstack.herokuapp.com/api"
 };
